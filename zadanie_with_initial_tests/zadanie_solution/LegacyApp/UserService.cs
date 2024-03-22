02a5ffafdc6b9588456d7291f7c39a3f26b2f0ae
Oliwier Szewko <s27540@pjwstk.edu.pl>
2024-03-22 09:15:08 +0100

    Dodanie metody checkTheAge

--- zadanie_with_initial_tests/zadanie_solution/LegacyApp/UserService.cs
+++ zadanie_with_initial_tests/zadanie_solution/LegacyApp/UserService.cs
@@ -16,11 +16,7 @@ namespace LegacyApp
                 return false;
             }
 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
+            if (!checkTheAge(dateOfBirth))
             {
                 return false;
             }
@@ -66,6 +62,20 @@ namespace LegacyApp
             }
 
             UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private static bool checkTheAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
             return true;
         }
     }
